889aa43049747fe992758ebda0e29f4a3a4a356e
Seung Bae <tmdqoek12@gmail.com>
2022-06-17 11:22:47 +0900

    초기 설정 push

---
+++ final-project-lego/src/main/java/org/kosta/finalproject/lego/FinalProjectLegoApplication.java
@@ -0,0 +1,13 @@
+package org.kosta.finalproject.lego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectLegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectLegoApplication.class, args);
+	}
+
+}
---
+++ final-project-lego/src/test/java/org/kosta/finalproject/lego/FinalProjectLegoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kosta.finalproject.lego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectLegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
