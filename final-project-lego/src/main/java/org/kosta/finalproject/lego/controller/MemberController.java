d47c6064bd57e9d0274debb66517b24cd1427a9c
Seunggyu-Kang <ksg_2342@naver.com>
2022-06-20 14:21:10 +0900

    member controller 수정

--- final-project-lego/src/main/java/org/kosta/finalproject/lego/controller/MemberController.java
+++ final-project-lego/src/main/java/org/kosta/finalproject/lego/controller/MemberController.java
@@ -1,8 +1,14 @@
 package org.kosta.finalproject.lego.controller;
 
+import org.kosta.finalproject.lego.mapper.MemberMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
+	private final MemberMapper memberMapper;
 
 }
