933234da2062bdf6b27f2417ae165b34a78ceaf8
Seunggyu-Kang <ksg_2342@naver.com>
2022-06-22 15:11:35 +0900

    mastermypage 2차 수정

---
+++ final-project-lego/src/main/java/org/kosta/finalproject/lego/controller/MasterMyPageController.java
@@ -0,0 +1,11 @@
+package org.kosta.finalproject.lego.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class MasterMyPageController {
+
+}
