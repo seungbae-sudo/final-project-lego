d7c043d57a7287d6b862cf62dfbbb1d836b28c50
Seung Bae <tmdqoek12@gmail.com>
2022-06-23 17:38:12 +0900

    고수 찾기 및 설문지 작성 1차 구현

---
+++ final-project-lego/src/main/java/org/kosta/finalproject/lego/mapper/SurveyMapper.java
@@ -0,0 +1,22 @@
+package org.kosta.finalproject.lego.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.kosta.finalproject.lego.vo.DaysVO;
+import org.kosta.finalproject.lego.vo.MasterVO;
+import org.kosta.finalproject.lego.vo.SkillsVO;
+import org.kosta.finalproject.lego.vo.TimesVO;
+
+@Mapper
+public interface SurveyMapper {
+	
+	List<SkillsVO> getSkills();
+
+	List<DaysVO> getDays();
+
+	List<TimesVO> getTimes();
+	
+	List<MasterVO> findMasterList(int[] skills, int[] days, int[] times,String categoryNo);
+	
+}
