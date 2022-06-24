7b7ef3b776f6484b70a752d2caa98ec03c548de4
Jiyeon-Park-dev <bakgii@naver.com>
2022-06-24 11:45:34 +0900

    layout 만 commit

--- final-project-lego/src/main/resources/jypSQL.sql
+++ final-project-lego/src/main/resources/jypSQL.sql
@@ -3,7 +3,51 @@ update member set password=#{password},name=#{name},address=#{address}, tel=#{te
 
 update member set password='aa',name='손석구',address='내 요동치는 마음속', tel='01012345678'
 			where id='java@naver.com'		
-			
+
+select*from board;
+
+select bc.category_no, m.id, b.board_title, b.hits
+from  board b, MEMBER m,board_category bc
+where bc.category_no=1
+and bc.category_no=b.category_no and m.id=b=id
+
+select r.category_no, r.board_title, r.hits,m.id
+from (
+	select bc.category_no, b.board_title, b.hits,b.id
+	from  board b, board_category bc
+	where bc.category_no=1) r ,member m
+where m.id=r.id
+
+select category_no, id, board_title, hits from board where category_no=1
+
+select r.board_no, r.board_title,r.hits, m.name
+from(
+	select b.board_no, b.board_title,b. hits, b.id
+	from  board b, board_category bc
+	where b.category_no=1
+	and b.category_no=bc.category_no
+) r, member m
+where  r.id=m.id
+
+select b.board_no, b.board_title,b.hits, m.name
+from board b, member m
+where b.category_no=1
+and b.id=m.id
+
+
+select b.board_no, b.board_title,b. hits
+	from  board b, board_category bc
+	where b.category_no=1
+	and b.category_no=bc.category_no
+
+
+--내가 쓴 글 리스트
+select b.boardno, b.boardtitle, bc.categoryname
+from  board b, boardcategory bc
+where b.categoryno=bc.categoryno(+)
+and b.id='lsj@naver.com'
+
+
 select*from member where id='java@naver.com'
 
 select*from master_detail
