7f1e08d4ca591715a16e49847c58752ad8bb2cb1
Seung Bae <tmdqoek12@gmail.com>
2022-06-24 17:36:01 +0900

    cart 1차 구현 (미완성)

---
+++ final-project-lego/src/main/java/org/kosta/finalproject/lego/mapper/CartMapper.java
@@ -0,0 +1,12 @@
+package org.kosta.finalproject.lego.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.kosta.finalproject.lego.vo.MasterVO;
+import org.kosta.finalproject.lego.vo.MemberVO;
+
+@Mapper
+public interface CartMapper {
+	
+	void addCart(String id, String masterId);
+	
+}
