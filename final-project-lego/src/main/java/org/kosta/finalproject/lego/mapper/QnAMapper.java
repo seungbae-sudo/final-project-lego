459334558b08eb147c90cae89972c56ba8d90ff0
Seung Bae <tmdqoek12@gmail.com>
2022-06-28 14:15:20 +0900

    QnA 리스트 쓰기 디테일 댓글 1차 구현

--- final-project-lego/src/main/java/org/kosta/finalproject/lego/mapper/QnAMapper.java
+++ final-project-lego/src/main/java/org/kosta/finalproject/lego/mapper/QnAMapper.java
@@ -1,5 +1,21 @@
 package org.kosta.finalproject.lego.mapper;
 
-public interface QnAMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.kosta.finalproject.lego.vo.QnACommentVO;
+import org.kosta.finalproject.lego.vo.QnAVO;
+
+@Mapper
+public interface QnAMapper {
+	
+	void writeQnA(QnAVO qnaVO);
+	
+	List<QnAVO> findQnAList();
+	
+	void writeQnAComment(QnACommentVO qnaCommentVO);
+	
+	QnAVO qnaDetail(int qnaNo);
+	
+	List<QnACommentVO> findQnACommentList();
 }
