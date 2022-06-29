58992ced00cef90490128201d2d064d6cab187a4
Seung Bae <tmdqoek12@gmail.com>
2022-06-29 17:34:57 +0900

    qna 수정 삭제 qna 글쓰기는 고객과 고수만 qna 답변은 adminaks 회원가입시 프로필 파일 업로드 업로드시 그 인원에 맞는 폴더를 생성후 그 하위에 이미지 파일 생성

--- final-project-lego/src/main/java/org/kosta/finalproject/lego/controller/MemberController.java
+++ final-project-lego/src/main/java/org/kosta/finalproject/lego/controller/MemberController.java
@@ -1,11 +1,21 @@
 package org.kosta.finalproject.lego.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.kosta.finalproject.lego.mapper.ImageMapper;
 import org.kosta.finalproject.lego.serivce.MasterService;
 import org.kosta.finalproject.lego.serivce.MemberService;
+import org.kosta.finalproject.lego.vo.ImageVO;
 import org.kosta.finalproject.lego.vo.MemberVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,18 +24,47 @@ import lombok.RequiredArgsConstructor;
 public class MemberController {
 	private final MemberService memberService;
 	private final MasterService masterService;
-	
+	private final ImageMapper imageMapper;
+
 	@PostMapping("/register")
-	public String register(MemberVO memberVO,String member) {
+	public String register(MemberVO memberVO, String member, HttpServletRequest request,
+			@RequestBody MultipartFile file) {
+		File folder = new File("C:\\finalproject\\final-project-lego\\final-project-lego\\src\\main\\resources\\static\\images\\"+memberVO.getId());
+		ImageVO imageVO = new ImageVO();
+		imageVO.setImageName(file.getOriginalFilename());
+		imageVO.setMemberVO(memberVO);
+		if (member.equals("1")) {
+			memberService.registerMember(memberVO);
+			imageMapper.uploadImage(imageVO);
+			try {
+				folder.mkdir();
+				file.transferTo(new File(
+						"C:\\finalproject\\final-project-lego\\final-project-lego\\src\\main\\resources\\static\\images\\"+memberVO.getId()+"\\"
+								+ file.getOriginalFilename()));
+
+			} catch (IllegalStateException | IOException e) {
+				e.printStackTrace();
+			}
 			
-		if(member.equals("1") ) {
-			memberService.registerMember(memberVO);	
+			System.out.println(file.getOriginalFilename());
 			return "redirect:/";
-		}else{		
+		} else {
 			masterService.registerMember(memberVO);
-			return "redirect:masterRegister?id="+memberVO.getId();
+			try {
+				folder.mkdir();
+				file.transferTo(new File(
+						"C:\\finalproject\\final-project-lego\\final-project-lego\\src\\main\\resources\\static\\images\\"+memberVO.getId()+"\\"
+								+ file.getOriginalFilename()));
+
+			} catch (IllegalStateException | IOException e) {
+				e.printStackTrace();
+			}
+			
+			return "redirect:masterRegister?id=" + memberVO.getId();
 		}
+
 	}
+
 	@RequestMapping("/loginFail")
 	public String loginFail() {
 		return "login_fail";
