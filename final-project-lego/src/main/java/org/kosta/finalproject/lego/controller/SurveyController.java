b2ce140cac0aa1d67a40bb1fa415535c87738f82
Seung Bae <tmdqoek12@gmail.com>
2022-07-05 01:30:57 +0900

    고수 찾기 pagination

--- final-project-lego/src/main/java/org/kosta/finalproject/lego/controller/SurveyController.java
+++ final-project-lego/src/main/java/org/kosta/finalproject/lego/controller/SurveyController.java
@@ -2,6 +2,7 @@ package org.kosta.finalproject.lego.controller;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -14,6 +15,7 @@ import org.kosta.finalproject.lego.vo.ImageVO;
 import org.kosta.finalproject.lego.vo.MasterDetailVO;
 import org.kosta.finalproject.lego.vo.MasterVO;
 import org.kosta.finalproject.lego.vo.MemberVO;
+import org.kosta.finalproject.lego.vo.Pagination;
 import org.kosta.finalproject.lego.vo.ReviewVO;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -44,15 +46,42 @@ public class SurveyController {
 	}
 
 	@RequestMapping("/findMasterList")
-	public String findMasterList(int[] skills, int[] days, int[] times, Model model, String categoryNo) {
+	public String findMasterList(int[] skills, int[] days, int[] times, Model model, String categoryNo,Pagination p,String pageNo) {
+		
+		if(pageNo==null) {// 클라이언트가 pageNo를 전달하지 않는 경우에는 첫 페이지를 보여준다.
+			p = new Pagination(surveyMapper.getTotalFindList(skills, days, times, categoryNo));
+		}else {
+			p = new Pagination(surveyMapper.getTotalFindList(skills, days, times, categoryNo), Integer.parseInt(pageNo));
+		}
 		System.out.println(categoryNo);
-		model.addAttribute("skllis", skills);
-		model.addAttribute("days", days);
-		model.addAttribute("times", times);
+		ArrayList<Object> skill = new ArrayList<Object>();
+		ArrayList<Object> day = new ArrayList<Object>();
+		ArrayList<Object> time = new ArrayList<Object>();
+		for(int i=0;i<skills.length;i++) {
+			skill.add(skills[i]);
+		}
+		for(int i=0;i<days.length;i++) {
+			day.add(days[i]);
+		}
+		for(int i=0;i<times.length;i++) {
+			time.add(times[i]);
+		}
+		List<MasterVO> list = surveyMapper.findMasterList(skills, days, times,categoryNo,p);
+		System.out.println(list.size());
+		List<ReviewVO> list2 = new ArrayList<ReviewVO>();
+		for(int i =0;i<list.size();i++) {
+			list2.addAll(surveyMapper.getScore(list.get(i).getId()));
+		}
+	
+		//System.out.println(skill);
+		model.addAttribute("skills", skill);
+		model.addAttribute("days", day);
+		model.addAttribute("times", time);
 		model.addAttribute("categoryNo", categoryNo);
-		model.addAttribute("masterList",surveyMapper.findMasterList(skills, days, times,categoryNo));
+		model.addAttribute("masterList",surveyMapper.findMasterList(skills, days, times,categoryNo,p));
 		model.addAttribute("count", surveyMapper.findcount(skills, days, times, categoryNo));
-		System.out.println(surveyMapper.findcount(skills, days, times, categoryNo));
+		model.addAttribute("pagination", p);
+		model.addAttribute("score", list2);
 		return "find-master-list";
 	}
 	
--- final-project-lego/src/main/java/org/kosta/finalproject/lego/mapper/SurveyMapper.java
+++ final-project-lego/src/main/java/org/kosta/finalproject/lego/mapper/SurveyMapper.java
@@ -1,5 +1,6 @@
 package org.kosta.finalproject.lego.mapper;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +8,8 @@ import org.apache.ibatis.annotations.Mapper;
 import org.kosta.finalproject.lego.vo.BookingVO;
 import org.kosta.finalproject.lego.vo.DaysVO;
 import org.kosta.finalproject.lego.vo.MasterVO;
+import org.kosta.finalproject.lego.vo.Pagination;
+import org.kosta.finalproject.lego.vo.ReviewVO;
 import org.kosta.finalproject.lego.vo.SkillsVO;
 import org.kosta.finalproject.lego.vo.TimesVO;
 
@@ -19,7 +22,7 @@ public interface SurveyMapper {
 
 	List<TimesVO> getTimes();
 	
-	List<MasterVO> findMasterList(int[] skills, int[] days, int[] times,String categoryNo);
+	List<MasterVO> findMasterList(int[] skills, int[] days, int[] times,String categoryNo,Pagination pagination);
 
 	List<MasterVO> findMasterList2(String [] id,int categoryNo);
 	
@@ -34,4 +37,8 @@ public interface SurveyMapper {
 	List<MasterVO> findMasterByKeyword(Map<String, String> map);
 	
 	Integer findcount(int[] skills, int[] days, int[] times,String categoryNo);
+	
+	Integer getTotalFindList(int[] skills, int[] days, int[] times,String categoryNo);
+	
+	List<ReviewVO> getScore(String id);
 }
